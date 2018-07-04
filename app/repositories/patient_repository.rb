5f76ae396ad758a64d038cf0bdbf01cad7e61c23
Carlos AP <carlos.agullo@gmail.com>
2018-07-03 23:38:46 -0400

    Added Repository Pattern. Adding class PatientRepository and methods

---
+++ app/repositories/patient_repository.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class PatientRepository < BaseRepository
+
+  class << self
+    def find_by_mrn(mrn)
+      klass.where('lower(mrn) = ?', mrn.downcase).first
+    end
+
+    def find_by_first_name(first_name)
+      klass.where('lower(first_name) = ?', first_name.downcase).first
+    end
+
+    def find_by_last_name(last_name)
+      klass.where('lower(last_name) = ?', last_name.downcase).first
+    end
+
+    def find_by_id!(id)
+      klass.find(id)
+    end
+
+    private
+
+    def klass
+      Patient
+    end
+  end
+end
