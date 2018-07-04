bf7b33451678ece1cba152be5dcadd96bf397f25
Carlos AP <carlos.agullo@gmail.com>
2018-07-03 22:41:44 -0400

    Adding uniqueness validation to mrn in patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,5 +2,5 @@ class Patient < ApplicationRecord
   has_many :encounters
   validates :first_name, :presence => true
   validates :last_name, :presence => true
-  validates :mrn, :presence => true
+  validates :mrn, :presence => true, :uniqueness => true
 end
