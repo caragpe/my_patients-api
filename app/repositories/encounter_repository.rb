589dfb314c3aa43e1bc8896dd45003b97cd1ad8b
Carlos AP <carlos.agullo@gmail.com>
2018-07-03 23:46:44 -0400

    Adding Encounter repository with basic methods

---
+++ app/repositories/encounter_repository.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class EncounterRepository < BaseRepository
+
+  class << self
+    def find_by_visit_number(visit_number)
+      klass.where('lower(visit_number) = ?', visit_number.downcase).first
+    end
+
+    def find_by_patient_id(patient_id)
+      klass.where('patient_id = ?', patient_id).first
+    end
+
+    def find_by_location(location)
+      klass.where('lower(location) = ?', location.downcase).first
+    end
+
+    def find_by_id!(id)
+      klass.find(id)
+    end
+
+    private
+
+    def klass
+      Encounter
+    end
+  end
+end
