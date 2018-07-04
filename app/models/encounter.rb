dbd92184bb1cfe97d9c9d263308761f31a3cac71
Carlos AP <carlos.agullo@gmail.com>
2018-07-03 22:31:39 -0400

    Adding validation

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -1,3 +1,7 @@
 class Encounter < ApplicationRecord
   belongs_to :patient
+
+  validates :visit_number, :presence => true
+  validates :admitted_at, :presence => true
+  validates :patient_id, :presence => true
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,6 @@
 class Patient < ApplicationRecord
   has_many :encounters
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :mrn, :presence => true
 end
