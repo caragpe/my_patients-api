d3b084d74aef9ebe8861bb0174b5b55cfb84f84f
Carlos AP <carlos.agullo@gmail.com>
2018-07-04 23:18:43 -0400

    Modifying Gemfile to mimic VC

--- test/factories/patient.rb
+++ test/factories/patient.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :patient do
     first_name Faker::Name.first_name
-    middle_name Faker::Name.middle_name
+    middle_name Faker::Name.first_name
     last_name Faker::Name.last_name
     weight 80
     height 1.80
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,23 @@
-ENV['RAILS_ENV'] ||= 'test'
+# frozen_string_literal: true
+
+require 'simplecov'
+SimpleCov.start
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+
+
+# Improved Minitest output (color and progress bar)
+require "minitest/reporters"
+Minitest::Reporters.use!(
+  Minitest::Reporters::ProgressReporter.new,
+  ENV,
+  Minitest.backtrace_filter)
+
 class ActiveSupport::TestCase
+  include FactoryBot::Syntax::Methods
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
