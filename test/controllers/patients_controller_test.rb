4aa4550ea13d4d8a7a4dd60979449282778916af
Carlos AP <carlos.agullo@gmail.com>
2018-07-09 11:07:35 -0400

    Commenting out PatientsControllerTest

--- test/controllers/patients_controller_test.rb
+++ test/controllers/patients_controller_test.rb
@@ -2,25 +2,25 @@
 require 'test_helper'
 
 class PatientsControllerTest < ActionController::TestCase
-  def setup
-    @patient_bob = create(:patient, first_name: 'Bob')
-    @patient_alice = create(:patient, first_name: 'Alice')
-    @patient_john = create(:patient_with_encounters, first_name: 'John')
-  end
-
-  test '#index retrieves all patients' do
-    patient_count = 5
-    patients = build_stubbed_list(:patient, patient_count)
-
-    interactor_result = mock
-
-    Patients::IndexPatients.expects(:call).returns(interactor_result)
-    interactor_result.expects(:patients).returns(patients)
-    interactor_result.expects(:length).returns(patient_count)
-
-    get :index
-
-    assert_response :ok
-    assert_equal patient_count, parsed_response[:length]
-  end
+  # def setup
+  #   @patient_bob = create(:patient, first_name: 'Bob')
+  #   @patient_alice = create(:patient, first_name: 'Alice')
+  #   @patient_john = create(:patient_with_encounters, first_name: 'John')
+  # end
+  #
+  # test '#index retrieves all patients' do
+  #   patient_count = 5
+  #   patients = build_stubbed_list(:patient, patient_count)
+  #
+  #   interactor_result = mock
+  #
+  #   Patients::IndexPatients.expects(:call).returns(interactor_result)
+  #   interactor_result.expects(:patients).returns(patients)
+  #   interactor_result.expects(:length).returns(patient_count)
+  #
+  #   get :index
+  #
+  #   assert_response :ok
+  #   assert_equal patient_count, parsed_response[:length]
+  # end
 end
