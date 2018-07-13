c9127f86160753cd590bea8f936a3e15b095ab04
Carlos AP <carlos.agullo@gmail.com>
2018-07-12 22:30:00 -0400

    Adding back listen gem. Setting root endpoint in config/routes.rb

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :development, :test do
 end
 
 group :development do
+  gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'rubocop', '~> 0.55.0'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :patients, only: [:index]
 
-  # get 'index' => 'patients#index'
+  root to: 'patients#index'
 end
