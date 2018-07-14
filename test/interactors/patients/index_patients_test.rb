4298b01b27a8949f9c970da96b599dbbe9ea8713
Carlos AP <carlos.agullo@gmail.com>
2018-07-14 00:10:02 -0400

    Improving IndexPatients interactor test (thanks Laith)

--- test/interactors/patients/index_patients_test.rb
+++ test/interactors/patients/index_patients_test.rb
@@ -4,14 +4,16 @@ require 'test_helper'
 
 class Patients::IndexPatientsTest < ActionController::TestCase
   test '#call returns list of patients' do
+    COLLECTION_SIZE = 4
     patients_collection = mock
-    patients_collection.expects(:length)
+    patients_collection.expects(:length).returns(COLLECTION_SIZE)
     PatientRepository.expects(:find_all).returns(patients_collection)
 
     result = Patients::IndexPatients.call
 
     assert result.success?
     assert_equal patients_collection, result.patients
+    assert_equal COLLECTION_SIZE, result.length
   end
 
   test '#call returns context.fail! is no patients are available' do
@@ -19,6 +21,6 @@ class Patients::IndexPatientsTest < ActionController::TestCase
 
     result = Patients::IndexPatients.call
 
-    assert !result.success?
+    refute result.success?
   end
 end
