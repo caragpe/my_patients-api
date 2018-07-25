0f79383b7c8a5ecb0154d9e1c0e348734eb16d54
Carlos AP <carlos.agullo@gmail.com>
2018-07-25 00:09:48 -0400

    Removing unnecessary test

--- test/interactors/patients/add_patient_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class Patients::AddPatientTest < ActionController::TestCase
-  test '#call adds a patient with correct params' do
-
-  end
-end
